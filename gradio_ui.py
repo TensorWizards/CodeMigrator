c1d8376db9f69ab156a1ec555ea159220255f942
dszvivian <dszvivian@gmail.com>
2024-05-09 22:29:53 +0530

    Updated UI

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,15 +1,16 @@
 import gradio as gr
 from main import codeConvert,codeExplain
 
-def process_files(files):
+def process_files(files,language):
     file_names = files[0]
     instructions =  codeExplain(file_names)
-    return codeConvert(instructions,"java")
+    generated_code = codeConvert(instructions,language)
+    return  [instructions, generated_code]
 
 demo = gr.Interface(
     process_files,
-    inputs='files',
-    outputs="markdown",
+    inputs=['files',gr.Dropdown(["python","java"])],
+    outputs=["markdown","markdown"],
     allow_flagging="never"
 )
 
