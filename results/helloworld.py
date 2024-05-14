6101e6de0d6257f3ed9aab5c40dbf8996cca004f
dszvivian <dszvivian@gmail.com>
2024-05-14 19:06:35 +0530

    added automated run file

--- results/helloworld.py
+++ results/helloworld.py
@@ -1,17 +1,1 @@
-# Import the COBOL module
-import cobol
-
-# Define the program ID
-program_id = "HELLO"
-
-# Define the procedure division
-procedure_division = """
-DISPLAY "HELLOWORLD".
-STOP RUN.
-"""
-
-# Create a new COBOL program
-program = cobol.Program(program_id, procedure_division)
-
-# Execute the program
-program.run()
+print("helloworld")
