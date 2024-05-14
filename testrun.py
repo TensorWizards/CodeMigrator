4e17da1c6c6fcaad126503cac7add5fc1472b924
dszvivian <dszvivian@gmail.com>
2024-05-14 23:29:37 +0530

    added Documentation for Generated Code

--- testrun.py
+++ testrun.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+from main import solve_error
 
 file_path = "./results/helloworld.py"
 
@@ -13,12 +14,20 @@ def run_file(filename):
             error_message = result.stderr
             return f"Error: {error_message}"
         
-        # If no error, return the output
-        output = result.stdout
-        return output
+        # # If no error, return the output
+        # output = result.stdout
+        return None
     except Exception as e:
         return f"Error: {str(e)}"
 
+code = ""
 
-print(run_file(file_path))
+with open(file_path, 'r') as file:
+    content = file.read()
+    code += content
+
+error_log = run_file(file_path)
+
+print(solve_error(code,error_log))
+
 
